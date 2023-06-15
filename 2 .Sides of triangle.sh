82c37e36b42827331963a9169986745afe467087
Maanas PK <116252079+Maanas10@users.noreply.github.com>
2023-06-15 14:56:35 +0530

    Sides of triangle.sh

--- 2 .Sides of triangle.sh
+++ 2 .Sides of triangle.sh
@@ -1,6 +1,6 @@
-if  [ $# -gt 1 ]
+if  [ $# -gt 3 -o $# -lt 3 -a $# -gt 0 ]
 then
-echo "syntax error"
+echo "syntax is $0 Enter exactly 3 sides [side1] [side2] [side3]"
 exit 1
 fi
 if [ $# -eq 0 ]
@@ -11,6 +11,10 @@ echo "Enter length of Second side of a triangle:"
 read t2
 echo "Enter length of third side of a triangle:"
 read t3
+else
+t1=$1
+t2=$2
+t3=$3
 fi
 if [ `expr $t1 + $t2` -le $t3 -o `expr $t1 + $t3` -le $t2 -o `expr $t2 + $t3` -le $t1 ]
 then
