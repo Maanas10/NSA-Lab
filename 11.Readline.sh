d6c17e5795138b1c464bf8b265e8711eadef97b3
Maanas PK <116252079+Maanas10@users.noreply.github.com>
2023-06-21 11:39:55 +0530

    Add files via upload

---
+++ 11.Readline.sh
@@ -0,0 +1,18 @@
+#shell script to print specified range of the lines from given files*
+
+if [ $# -le 2 ]
+then
+echo "Syntax is <$0> [range] [files........]"
+exit 1
+fi
+n1=$1
+n2=$2
+shift
+shift
+n3=`expr $n2 - $n1 +1`
+while [ $# -ne 0 ]
+do
+head -$n2 $1 | tail - $n3
+shift 
+done 
+exit 0 
