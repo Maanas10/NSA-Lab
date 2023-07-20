bec58d01f89d8b808ea5cee579adade003027536
Maanas PK <116252079+Maanas10@users.noreply.github.com>
2023-07-20 10:42:58 +0530

    Add files via upload

---
+++ 10.sh
@@ -0,0 +1,17 @@
+if [ $# -ne 0 ]
+then
+echo "Syntax error"
+exit 1
+fi
+echo "\n\nDirectory of 'pwd' \n "
+for f in *
+do
+fdt=`date -r $f "+ %d-%m-%y %H:%M:%S"`
+if [ -d $f ]
+then
+echo "$f\t\t<DIR>\t$fdt"
+else
+fsize=`cat $f | wc -c`
+echo "$f\t\t $fsize \t $fdt"
+fi
+done
